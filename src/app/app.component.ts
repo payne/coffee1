ae727227273a531542b1598be59c8a22dccd6da1
Matt Payne <Payne@MattPayne.org>
2018-07-21 19:13:54 -0500

    s/c1-root/app-c1-root/g

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,7 +1,7 @@
 import { Component } from '@angular/core';
 
 @Component({
-  selector: 'c1-root',
+  selector: 'app-c1-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
